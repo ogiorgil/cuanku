84316c852a9276c09df6aaab1fbd85045c320955
David Alexander <david.alexander01@ui.ac.id>
2022-11-23 10:23:03 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/akmr/cuanku/CuankuApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.akmr.cuanku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuankuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuankuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/akmr/cuanku/CuankuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.akmr.cuanku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuankuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
